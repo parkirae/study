b7b87d711b8f54aa7dc8e341e9e088dd0e0ee6f6
parkirae <parkirae@kakao.com>
2022-11-18 18:31:43 +0900

    Feat :art:: 게시글 수정 기능 구현
    Body: index.js update function() 추가했습니다.

--- SpringBoot_AWS/freelec-springboot2-webservice/src/main/resources/static/js/app/index.js
+++ SpringBoot_AWS/freelec-springboot2-webservice/src/main/resources/static/js/app/index.js
@@ -10,9 +10,9 @@ var main = {
             title: $('#title').val(),
             author: $('#author').val(),
             content: $('#content').val()
-        };
+        }
 
-        $.ajax({
+            $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
@@ -24,7 +24,29 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    }
+    },
+
+    update : function () {
+            var data = {
+                title: $('#title').val(),
+                content: $('#content').val()
+            };
+
+            var id = $('#id').var();
+
+            $.ajax({
+                type: 'PUT',
+                url: '/api/v1/posts/' + id,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function() {
+                alert('글이 수정되었습니다.');
+                window.location.href = '/';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        }
 
 };
 
