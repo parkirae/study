fcc40e5a6a860eee9332e1c3bec31a95ff767961
parkirae <parkirae@kakao.com>
2022-11-18 19:35:56 +0900

    Depend :heavy_plus_sign:: oauth2-client
    Body: Social Login을 위한 라이브러리입니다.

--- SpringBoot_AWS/freelec-springboot2-webservice/build/resources/main/static/js/app/index.js
+++ SpringBoot_AWS/freelec-springboot2-webservice/build/resources/main/static/js/app/index.js
@@ -53,6 +53,21 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset-utf-8'
+        }).done(function(){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
     }
 };
 
