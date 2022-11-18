02ef862206ff40b24987735c8fb6cdf967f92852
parkirae <parkirae@kakao.com>
2022-11-18 18:12:08 +0900

    Feat :art:: PostsService findAllDesc() 추가
    Body: 해당 메서드는 PostRepository의 결과로 넘어온 Posts의 Stream을 map을 통해 Dto -> List로 반환합니다.
    
    readOnly = false는 조회 속도 개선을 위함입니다.

--- SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -2,14 +2,17 @@ package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
-import jdk.internal.net.http.common.Log;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,5 +42,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = false)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
