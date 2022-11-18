c23fcc8afdd89db5a8d34ed013aeff3fb9fc4c13
parkirae <parkirae@kakao.com>
2022-11-18 11:09:19 +0900

    Test :white_check_mark:: HelloControllerTest
    Body: HelloControllerTest 단위 테스트입니다.
    
    Get으로 "Hello"를 요청했을 때 반환되는 값을 확인합니다

---
+++ SpringBoot_AWS/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)    // SpringBootTest와 JUnit 연결자 역할을 합니다.
+@WebMvcTest(controllers = HelloController.class)    // Web에 집중할 수 있는 어노테이션입니다.
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;    // web API를 테스트할 때 사용합니다.
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
