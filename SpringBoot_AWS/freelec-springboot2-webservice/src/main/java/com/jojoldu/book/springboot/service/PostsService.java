029a8ebcd8d93cf3e134ff6077700bc883fec399
parkirae <parkirae@kakao.com>
2022-11-18 19:27:47 +0900

    Feat :art:: 게시글 삭제 기능 구현
    Body: PostsService delete() 추가했습니다.

--- SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,5 +48,13 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
