96eea54017ce92470c6d1dc3b3cb6dfc44d7cd67
parkirae <parkirae@kakao.com>
2022-11-18 13:14:22 +0900

    Feat :art:: PostsApiController update 추가
    Body: update는 id값으로 게시글을 수정합니다.

--- SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/controller/PostsApiController.java
+++ SpringBoot_AWS/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/controller/PostsApiController.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web.controller;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +16,10 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
