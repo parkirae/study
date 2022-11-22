220994e9583648add41f8f80dd677ac9f25f332f
parkirae <parkirae@kakao.com>
2022-11-22 10:44:12 +0900

    :tada: JPA_QUICKSTART Initial Commit

---
+++ JPA_QUICKSTART/src/test/java/com/rubypaper/shopping/JpaQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubypaper.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
