f58ac907c55ae7cebb0f62d5e5ef7920a94b42a7
parkirae <parkirae@kakao.com>
2022-11-23 07:03:11 +0900

    Feat :art:: ProductRepository 작성

---
+++ JPA_QUICKSTART/src/main/java/com/rubypaper/shopping/biz/repository/ProductRepository.java
@@ -0,0 +1,34 @@
+package com.rubypaper.shopping.biz.repository;
+
+import com.rubypaper.shopping.biz.domain.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class ProductRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // 상품 등록 또는 수정
+    public void insertOrUpdateProduct(Product product) {
+        // 매개변수로 전달된 상품에 아이디 존재 여부에 따라서 등록/수정이 분기 처리 됩니다.
+        if (product.getId() == null) {
+            em.persist(product);
+        } else {
+            em.merge(product);
+        }
+    }
+
+    // 상품 상세 조회
+    public Product getProduct(Long id) {
+        return em.find(Product.class, id);
+    }
+
+    // 상품 목록 조회
+    public List<Product> getProductList() {
+        return em.createQuery("SELECT p FROM Product p ORDER BY p.id",
+                Product.class).getResultList();
+    }
+}
