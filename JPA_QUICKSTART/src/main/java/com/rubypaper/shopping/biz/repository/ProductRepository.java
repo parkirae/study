d8112651b4614100803f34e53ae6b1cf9c457546
parkirae <parkirae@kakao.com>
2022-11-23 07:20:50 +0900

    Refactor :hammer:: Repository @Repository 추가

--- JPA_QUICKSTART/src/main/java/com/rubypaper/shopping/biz/repository/ProductRepository.java
+++ JPA_QUICKSTART/src/main/java/com/rubypaper/shopping/biz/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.rubypaper.shopping.biz.repository;
 
 import com.rubypaper.shopping.biz.domain.Product;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Repository
 public class ProductRepository {
 
     @PersistenceContext
