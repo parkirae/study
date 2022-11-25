77e78918aa7f59f054ed7a452784ce63fcbd46b4
parkirae <parkirae@kakao.com>
2022-11-26 08:39:36 +0900

    Feat :art:art:art:: apiService.js 작성
    Body: apiService는 백엔드와 프론트엔드를 연결할 때 사용하는 유틸리티 함수입니다.

---
+++ React_SpringBoot_AWS/todo-app_FE/src/ApiService.js
@@ -0,0 +1,25 @@
+import { API_BASE_URL } from './api-config';
+
+export function call(api, method, request) {
+  let options = {
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    url: API_BASE_URL + api,
+    method: method,
+  };
+  if (request) {
+    // GET
+    options.body = JSON.stringify(request);
+  }
+  return fetch(options.url, options)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    })
+    .catch((error) => {
+      console.log('http error');
+      console.logo(error);
+    });
+}
