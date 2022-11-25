a6881d6074cd557faa86bd349d5c11e167af858f
parkirae <parkirae@kakao.com>
2022-11-25 12:24:58 +0900

    Feat :art::art:: Todo.js checkboxEventHandler() 함수 적용

--- React_SpringBoot_AWS/todo-react-app/src/Todo.js
+++ React_SpringBoot_AWS/todo-react-app/src/Todo.js
@@ -14,6 +14,11 @@ const Todo = (props) => {
   const [readOnly, setReadOnly] = useState(true);
   const editItem = props.editItem;
 
+  const checkboxEventHandler = (e) => {
+    item.done = e.target.checked;
+    editItem();
+  };
+
   const editEventHandler = (e) => {
     item.title = e.target.value;
     editItem();
@@ -37,7 +42,7 @@ const Todo = (props) => {
 
   return (
     <ListItem>
-      <Checkbox checked={item.done} />
+      <Checkbox checked={item.done} onChange={checkboxEventHandler} />
       <ListItemText>
         <InputBase
           inputProps={{ 'aria-label': 'naked', readOnly: readOnly }}
