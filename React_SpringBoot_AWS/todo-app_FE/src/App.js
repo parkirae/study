293ab23ffd770884c149920bb837c6f9725cb449
parkirae <parkirae@kakao.com>
2022-11-25 17:31:08 +0900

    Feat :art::art::art:: 서비스 통합
    Body: fetch()를 통해 백엔드 서버로 요청을 보냅니다. 이때 프론트엔드 서버를 동작하고, 브라우저 콘솔 창에 CORS 에러 메세지가 발생합니다.
    
    처음 리소스를 제공한 도메인(Origin, FE)이 현재 요청하려는 도메인(BE)과 다르기 때문에 CORS 에러가 발생합니다.
    
    이후 백엔드 서버에서 CORS 허락 요청을 처리합니다.

--- React_SpringBoot_AWS/todo-app_FE/src/App.js
+++ React_SpringBoot_AWS/todo-app_FE/src/App.js
@@ -7,6 +7,20 @@ import AddTodo from './AddTodo';
 function App() {
   const [items, setItems] = useState([]);
 
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  fetch('http://localhost:8080/todo', requestOptions)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        setItems(response.data);
+      },
+      (error) => {}
+    );
+
   const addItem = (item) => {
     item.id = 'ID-' + items.length;
     item.done = false;
