206f0ae8c56e7585e4bbd382c8e192b598b0ce49
parkirae <parkirae@kakao.com>
2022-11-25 11:27:26 +0900

    Feat :art:: App.js에 AddTojo.js 추가

--- React_SpringBoot_AWS/todo-react-app/src/App.js
+++ React_SpringBoot_AWS/todo-react-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Todo from './Todo';
 import React, { useState } from 'react';
-import { List, Paper } from '@mui/material';
+import { Container, List, Paper } from '@mui/material';
+import AddTodo from './AddTodo';
 
 function App() {
   const [items, setItems] = useState([
@@ -27,7 +28,14 @@ function App() {
     </Paper>
   );
 
-  return <div className="App">{todoItems}</div>;
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        <AddTodo />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
