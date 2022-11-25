a3c2b620228058f2d63ba1e15c7ae7dcb631d028
parkirae <parkirae@kakao.com>
2022-11-25 11:31:50 +0900

    Feat :art:: AddTojo.js onInputChange 추가
    Body: onInputChange는 사용자의 값을 입력하면 변화를 감지하고 해당 값을 title에 저장합니다.
    
    title은 item으로 들어갑니다.

--- React_SpringBoot_AWS/todo-react-app/src/AddTodo.js
+++ React_SpringBoot_AWS/todo-react-app/src/AddTodo.js
@@ -5,10 +5,20 @@ import { Button, Grid, TextField } from '@mui/material';
 const AddTodo = (props) => {
   const [item, setItem] = useState({ title: '' });
 
+  const onInputChange = (e) => {
+    setItem({ title: e.target.value });
+    console.log(item);
+  };
+
   return (
     <Grid container style={{ marginTop: 20 }}>
       <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-        <TextField placeholder="Add Todo here" fullWidth />
+        <TextField
+          placeholder="Add Todo here"
+          fullWidth
+          onChange={onInputChange}
+          value={item.title}
+        />
       </Grid>
       <Grid xs={1} md={1} item>
         <Button
