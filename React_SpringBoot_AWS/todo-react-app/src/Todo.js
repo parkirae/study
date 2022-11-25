6e89446a2527d6746ace5551beb45e6b252f1057
parkirae <parkirae@kakao.com>
2022-11-25 09:07:42 +0900

    Conf :wrench:: prettier 적용

--- React_SpringBoot_AWS/todo-react-app/src/Todo.js
+++ React_SpringBoot_AWS/todo-react-app/src/Todo.js
@@ -6,7 +6,7 @@ const Todo = () => {
       <input type="checkbox" id="todo0" name="todo0" value="todo0" />
       <label for="todo0">Todo 컴포넌트 만들기</label>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
