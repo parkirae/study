ed920b51e2f010da633a08461ce7d6b0f9e76767
parkirae <parkirae@kakao.com>
2022-11-26 17:03:26 +0900

    Feat :art:: UserController 회원 가입(signup) 작성

---
+++ React_SpringBoot_AWS/todo-app_BE/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // 회원 가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            if (userDTO == null || userDTO.getPassword() == null) {
+                throw new RuntimeException("Invalid Password value.");
+            }
+            // 요청을 이용해 유저를 저장합니다.
+            UserEntity user = UserEntity.builder()
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+            // 서비스를 이용해 리포지터리에 유저를 저장합니다.
+            UserEntity registeredUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .id(registeredUser.getId())
+                    .username(registeredUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDTO);
+        }
+    }
+}
