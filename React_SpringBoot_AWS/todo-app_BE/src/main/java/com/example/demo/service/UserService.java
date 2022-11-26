e43b941cdfcba7a14daccf3bb80fc28c6cd20c5a
parkirae <parkirae@kakao.com>
2022-11-26 16:56:59 +0900

    Feat :art:: UserService 생성 기능 구현

---
+++ React_SpringBoot_AWS/todo-app_BE/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.model.UserEntity;
+import com.example.demo.persistence.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 생성
+    public UserEntity create(final UserEntity userEntity) {
+        if (userEntity == null || userEntity.getUsername() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String username = userEntity.getUsername();
+        if (userRepository.existsByUsername(username)) {
+            log.warn("Username already exists {}", username);
+            throw new RuntimeException("Username already exists");
+        }
+
+        return userRepository.save(userEntity);
+    }
+}
