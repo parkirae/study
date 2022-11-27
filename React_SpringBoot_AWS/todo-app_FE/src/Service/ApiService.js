1decdf08c727a32ded8dc413a1798df851fc6c45
parkirae <parkirae@kakao.com>
2022-11-27 14:09:59 +0900

    Feat :art:: ApiService.js 토큰을 헤더에 저장하는 기능 추가

--- React_SpringBoot_AWS/todo-app_FE/src/Service/ApiService.js
+++ React_SpringBoot_AWS/todo-app_FE/src/Service/ApiService.js
@@ -1,13 +1,22 @@
 import { API_BASE_URL } from '../api-config';
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    'Content-Type': 'application/json',
+  });
+
+  // local storage에서 ACCESS TOKEN을 가져옵니다.
+  const accessToken = localStorage.getItem('ACCESS_TOKEN');
+  if (accessToken && accessToken != null) {
+    headers.append('Authorization', 'Bearer ' + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      'Content-Type': 'application/json',
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
+
   if (request) {
     // GET
     options.body = JSON.stringify(request);
