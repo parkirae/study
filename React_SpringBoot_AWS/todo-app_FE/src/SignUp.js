2b661797940416924794bf682719d908e65c3eff
parkirae <parkirae@kakao.com>
2022-11-27 14:47:48 +0900

    Feat :art:: Signup.js 작성

---
+++ React_SpringBoot_AWS/todo-app_FE/src/SignUp.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Container, Grid, Typography, TextField, Button } from '@mui/material';
+import { signup } from './Service/ApiService';
+import { Link } from 'react-router-dom';
+import SignUp from './SignUp';
+
+function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 오브젝트에서 form에 저장된 데이터를 맵의 형태로 변경합니다.
+    const data = new FormData(event.target);
+    const username = data.get('username');
+    const password = data.get('password');
+    signup({ username: username, password: password }).then((response) => {
+      // 계정 생성에 성공하면 로그인 페이지로 이동합니다.
+      window.location.href = '/';
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" style={{ marginTop: '8%' }}>
+      <form noValidate onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography component="h1" variant="h5">
+              계정 생성
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              autoComplete="fname"
+              name="username"
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="아이디"
+              autoFocus
+            />
+          </Grid>
+          <Gird item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="패스워드"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Gird>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              계정 생성
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link to="/login" variant="body2">
+              이미 계정이 있다면 로그인 하세요! :)
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+}
+
+export default SignUp;
