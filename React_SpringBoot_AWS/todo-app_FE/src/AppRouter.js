e30726786e450cd0e4ec3197bc59bc69f28b818d
parkirae <parkirae@kakao.com>
2022-11-27 14:49:25 +0900

    Feat :art:: AppRouter.js SignUp 라우트 추가

--- React_SpringBoot_AWS/todo-app_FE/src/AppRouter.js
+++ React_SpringBoot_AWS/todo-app_FE/src/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import SignUp from './SignUp';
 import Login from './Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Typography, Box } from '@mui/material';
@@ -22,6 +23,7 @@ function AppRouter() {
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
       <Box mt={5}>
