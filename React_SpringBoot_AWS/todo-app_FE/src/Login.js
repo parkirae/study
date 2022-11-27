98d4ebc96b6bb722e2992765c74ce3db01258652
parkirae <parkirae@kakao.com>
2022-11-27 13:53:37 +0900

    Feat :art:: Login.js 수정

--- React_SpringBoot_AWS/todo-app_FE/src/Login.js
+++ React_SpringBoot_AWS/todo-app_FE/src/Login.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { Container, Grid, Typography, TextField, Button } from '@mui/material';
+import { signin } from './Service/ApiService';
 
 const Login = () => {
-  return <p>로그인 페이지</p>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const username = data.get('username');
+    const password = data.get('password');
+    // ApiService signin을 이용해 로그인합니다.
+    signin({ username: username, password: password });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" style={{ marginTop: '8%' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+        </Grid>
+      </Grid>
+      <form noValidate onSubmit={handleSubmit}>
+        {' '}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="아이디"
+              name="username"
+              autoComplete="username"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="패스워드"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              로그인
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 };
 
 export default Login;
