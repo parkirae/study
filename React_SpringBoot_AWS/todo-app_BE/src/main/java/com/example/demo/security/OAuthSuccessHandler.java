f7712c579e186c30767c4d8865e4fe21c38e962a
parkirae <parkirae@kakao.com>
2022-12-01 12:22:27 +0900

    Feat :art:: OAuthSuccessHandler redirect_url 리디렉트 추가

--- React_SpringBoot_AWS/todo-app_BE/src/main/java/com/example/demo/security/OAuthSuccessHandler.java
+++ React_SpringBoot_AWS/todo-app_BE/src/main/java/com/example/demo/security/OAuthSuccessHandler.java
@@ -6,22 +6,31 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private static final String LOCAL_REDIRECT_URL = "http://localhost:3000";
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        log.info("auth succeeded");
         TokenProvider tokenProvider = new TokenProvider();
         String token = tokenProvider.create(authentication);
 
-        response.getWriter().write(token);
+        Optional<Cookie> oCookie = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals(LOCAL_REDIRECT_URL)).findFirst();
+        Optional<String> redirectUri = oCookie.map(Cookie::getValue);
+
         log.info("token {}", token);
+        response.sendRedirect(redirectUri.orElseGet(() -> LOCAL_REDIRECT_URL) + "/sociallogin?token=" + token);
         response.sendRedirect("http://localhost:3000" + token);
     }
 }
