cf570c60b7ee41e2c0559cb485bc0feb16f06410
parkirae <parkirae@kakao.com>
2022-12-10 08:37:43 +0900

    Feat :art:: HikariCP 설정
    Body: HikariCP 설정은 ConnectionUtil에 작성했습니다.
    
    해당 클래스는 enum으로 하나의 객체만 생성하여 사용합니다.

--- Java_Develop/jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
+++ Java_Develop/jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -1,5 +1,7 @@
 package org.zerock.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +9,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectTests {
+
+    @Test
+    public void testHikariCP() throws Exception {
+
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Drvier");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser"); // 개인 mariaDB ID
+        config.setPassword("webuser"); // 개인 mariaDB PW
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
+
+        connection.close();
+    }
 
     @Test
     public void testConnection() throws Exception {
