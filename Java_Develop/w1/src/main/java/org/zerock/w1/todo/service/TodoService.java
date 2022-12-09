c436ac018c86a9296b5ab8a82bf5e9c57b88c845
parkirae <parkirae@kakao.com>
2022-12-09 10:37:35 +0900

    Feat :art:: TodoService 등록, 조회 기능 추가
    Body: 등록, 조회는 각각 register, getList 메서드입니다.
    
    TodoDTO 객체를 반환합니다.

--- Java_Develop/w1/src/main/java/org/zerock/w1/todo/service/TodoService.java
+++ Java_Develop/w1/src/main/java/org/zerock/w1/todo/service/TodoService.java
@@ -1,5 +1,31 @@
 package org.zerock.w1.todo.service;
 
+import org.zerock.w1.todo.dto.TodoDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public enum TodoService {
     INSTANCE;
+
+    public void register(TodoDTO todoDTO) {
+        System.out.println("DEBUG... " + todoDTO);
+    }
+
+    // 조회
+    public List<TodoDTO> getList() {
+
+        List<TodoDTO> todoDTOS = IntStream.range(0, 10).mapToObj(i -> {
+            TodoDTO dto = new TodoDTO();
+            dto.setTno((long)i);
+            dto.setTitle("Todo.." + i);
+            dto.setDueDate(LocalDate.now());
+
+            return dto;
+        }).collect(Collectors.toList());
+
+        return todoDTOS;
+    }
 }
