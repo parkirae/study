2e14424ba6072ad855b8537f72832e10ffe65f84
parkirae <parkirae@kakao.com>
2022-12-10 08:55:24 +0900

    Feat :art::art::art:: TodoDAO 등록 기능 추가

--- Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/dao/TodoDAO.java
+++ Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/dao/TodoDAO.java
@@ -1,8 +1,10 @@
 package com.zerock.jdbcex.dao;
 
+import com.zerock.jdbcex.domain.TodoVO;
 import lombok.Cleanup;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -39,5 +41,21 @@ public class TodoDAO {
         String now = resultSet.getString(1);
 
         return now;
+    }
+
+    // 등록
+    public void insert(TodoVO vo) throws Exception {
+        String sql = "INSERT INTO tbl_todo(title, dueDate, finished) VALUES(?, ?, ?)";
+
+        @Cleanup
+        Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, vo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(vo.getDueDate()));
+        preparedStatement.setBoolean(3, vo.isFinished());
+
+        preparedStatement.executeQuery();
     }
 }
