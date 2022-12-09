a75ca183703ada301b7161e7490a4b7d0155cb6b
parkirae <parkirae@kakao.com>
2022-12-10 08:58:00 +0900

    Test ✅✅✅: TodoDAO insert 테스트
    Body: TodoVO에서 실수로 빠졌던 @Builder 추가하였습니다.

--- Java_Develop/jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
+++ Java_Develop/jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -1,8 +1,11 @@
 package org.zerock.dao;
 
 import com.zerock.jdbcex.dao.TodoDAO;
+import com.zerock.jdbcex.domain.TodoVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 public class TodoDAOTests {
 
@@ -16,5 +19,15 @@ public class TodoDAOTests {
     @Test
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2022, 12, 31))
+                .build();
+
+        todoDAO.insert(todoVO);
     }
 }
