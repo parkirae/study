3443d8f53333ee7c393b51a80305fa35eaa1c345
parkirae <parkirae@kakao.com>
2022-12-10 09:56:25 +0900

    Feat :art:: TodoListController 수정

--- Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/controller/TodoListController.java
+++ Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/controller/TodoListController.java
@@ -1,5 +1,6 @@
 package com.zerock.jdbcex.controller;
 
+import com.zerock.jdbcex.dto.TodoDTO;
 import com.zerock.jdbcex.service.TodoService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
+import java.util.List;
 
 @Log4j2
 @WebServlet(name = "todoListController", value = "/todo/list")
@@ -19,5 +21,14 @@ public class TodoListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("todo list...");
+
+        try {
+            List<TodoDTO> dtoList = todoService.listAll();
+            req.setAttribute("dtoList", dtoList);
+            req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("list error");
+        }
     }
 }
