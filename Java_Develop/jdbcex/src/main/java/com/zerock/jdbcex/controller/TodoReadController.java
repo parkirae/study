b80f1dc5de2a0614f71d9ec1d996bc5554ef98a0
parkirae <parkirae@kakao.com>
2022-12-10 10:43:11 +0900

    Feat :art:: TodoReadController 추가

---
+++ Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/controller/TodoReadController.java
@@ -0,0 +1,36 @@
+package com.zerock.jdbcex.controller;
+
+import com.zerock.jdbcex.dto.TodoDTO;
+import com.zerock.jdbcex.service.TodoService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.IOException;
+
+@Log4j2
+@WebServlet(name = "todoReadController", value = "/todo/read")
+public class TodoReadController extends HttpServlet {
+
+    private TodoService todoService = TodoService.INSTNACE;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            Long tno = Long.parseLong(req.getParameter("tno"));
+
+            TodoDTO todoDTO = todoService.get(tno);
+
+            req.setAttribute("dto", todoDTO);
+
+            req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("read error");
+        }
+    }
+}
