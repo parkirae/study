c012877f3f49f73a3897575708747967f7503254
parkirae <parkirae@kakao.com>
2022-12-10 14:21:46 +0900

    Feat :art:: LoginController 추가

--- Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/LoginController.java
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/LoginController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.java.Log;
 
 import java.io.IOException;
@@ -17,6 +18,17 @@ public class LoginController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         log.info("login get...");
+
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String str = mid + mpw;
+
+        HttpSession session = req.getSession();
+
+        session.setAttribute("loginInfo", str);
+
+        resp.sendRedirect("/todo/list");
 
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
     }
