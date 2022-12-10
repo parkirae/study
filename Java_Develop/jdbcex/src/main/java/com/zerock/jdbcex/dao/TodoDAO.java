bf7ec2de806272a117342272c7fb9ae2702cee10
parkirae <parkirae@kakao.com>
2022-12-10 09:12:21 +0900

    Feat :art::art::art:: TodoDAO 단건 조회 기능 추가

--- Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/dao/TodoDAO.java
+++ Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/dao/TodoDAO.java
@@ -88,4 +88,30 @@ public class TodoDAO {
 
         return list;
     }
+
+    // 단건 조회
+    public TodoVO selectOne(Long tno) throws Exception {
+
+        String sql = "SELECT * FROM tbl_todo WHERE tno = ?";
+
+        @Cleanup
+        Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setLong(1, tno);
+
+        @Cleanup
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+        TodoVO vo = TodoVO.builder()
+                .tno(resultSet.getLong("tno"))
+                .title(resultSet.getString("title"))
+                .dueDate(resultSet.getDate("dueDate").toLocalDate())
+                .finished(resultSet.getBoolean("finished"))
+                .build();
+
+        return vo;
+    }
 }
