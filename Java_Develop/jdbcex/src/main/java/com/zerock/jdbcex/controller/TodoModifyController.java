4ff32f8bcbe55bc04581fe841ae2c18db8f58b0e
parkirae <parkirae@kakao.com>
2022-12-10 11:05:18 +0900

    Feat :art:: TodoModifyController doPost 추가

--- Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/controller/TodoModifyController.java
+++ Java_Develop/jdbcex/src/main/java/com/zerock/jdbcex/controller/TodoModifyController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Log4j2
@@ -32,5 +33,26 @@ public class TodoModifyController extends HttpServlet {
             log.error(e.getMessage());
             throw new ServletException("modify get... error");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String finishedStr = req.getParameter("finished");
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .tno(Long.parseLong(req.getParameter("tno")))
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"), DATEFORMATTER))
+                .finished(finishedStr != null && finishedStr.equals("on"))
+                .build();
+
+        log.info("/todo/modify POST...");
+        log.info(todoDTO);
+        try {
+            todoService.modify(todoDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resp.sendRedirect("/todo/list");
     }
 }
