401929eaed97578cd68d7e4457167f956fbb7d09
parkirae <parkirae@kakao.com>
2022-12-10 14:13:01 +0900

    Feat :art:: 등록 시 로그인 여부 확인

--- Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/TodoRegisterController.java
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/TodoRegisterController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
@@ -23,6 +24,21 @@ public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("/todo/register GET...");
+
+        HttpSession session = req.getSession();
+
+        if (session.isNew()) {
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자입니다.");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        if (session.getAttribute("loginInfo") == null) {
+            log.info("로그인한 정보가 없는 사용자입니다.");
+            resp.sendRedirect("/login");
+            return;
+        }
+
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
