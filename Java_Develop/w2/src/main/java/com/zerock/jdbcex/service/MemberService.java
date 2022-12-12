baa750c9bf5c722f740e34b38b3115927da2fe42
parkirae <parkirae@kakao.com>
2022-12-13 02:31:59 +0900

    Feat :art:: MemberService 추가

---
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/service/MemberService.java
@@ -0,0 +1,32 @@
+package com.zerock.jdbcex.service;
+
+import com.sun.javadoc.MemberDoc;
+import com.zerock.jdbcex.dao.MemberDAO;
+import com.zerock.jdbcex.domain.MemberVO;
+import com.zerock.jdbcex.dto.MemberDTO;
+import com.zerock.jdbcex.util.MapperUtil;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+
+@Log4j2
+public enum MemberService {
+
+    INSTANCE;
+
+    private MemberDAO dao;
+    private ModelMapper modelMapper;
+
+    MemberService() {
+        dao = new MemberDAO();
+        modelMapper = MapperUtil.INSTANCE.get();
+    }
+
+    public MemberDTO login(String mid, String mpw) throws Exception {
+
+        MemberVO vo = dao.getWithPassword(mid, mpw);
+
+        MemberDTO memberDTO = modelMapper.map(vo, MemberDTO.class);
+
+        return memberDTO;
+    }
+}
