82e7a21c498abae5a5adfad53f88255bfe2c2543
parkirae <parkirae@kakao.com>
2022-12-13 03:39:31 +0900

    Feat :art:: MemberDAO updateUuid 추가

--- Java_Develop/w2/src/main/java/com/zerock/jdbcex/dao/MemberDAO.java
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/dao/MemberDAO.java
@@ -36,4 +36,19 @@ public class MemberDAO {
 
         return memberVO;
     }
+
+    public void updateUuid(String mid, String uuid) throws Exception {
+
+        String sql = "UPDATE tbl_member SET uuid =? WHERE mid = ?";
+
+        @Cleanup
+        Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, uuid);
+        preparedStatement.setString(2, mid);
+
+        preparedStatement.executeUpdate();
+    }
 }
