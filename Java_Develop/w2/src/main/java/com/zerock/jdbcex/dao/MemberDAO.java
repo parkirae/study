3b4039511a07efe05975d56f84bca2a4a2b3488a
parkirae <parkirae@kakao.com>
2022-12-13 02:28:33 +0900

    Feat :art:: MemberDAO 추가

---
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/dao/MemberDAO.java
@@ -0,0 +1,39 @@
+package com.zerock.jdbcex.dao;
+
+import com.zerock.jdbcex.domain.MemberVO;
+import lombok.Cleanup;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+
+    public MemberVO getWithPassword(String mid, String mpw) throws Exception {
+
+        String query = "SELECT mid, mpw, mname FROM tbl_member WHERE mid =? and mpw = ?";
+
+        MemberVO memberVO = null;
+
+        @Cleanup
+        Connection connection = ConnectionUtil.INSTANCE.getConnection();
+
+        @Cleanup
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, mid);
+        preparedStatement.setString(2, mpw);
+
+        @Cleanup
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        memberVO = MemberVO.builder()
+                .mid(resultSet.getString(1))
+                .mpw(resultSet.getString(2))
+                .mname(resultSet.getString(3))
+                .build();
+
+        return memberVO;
+    }
+}
