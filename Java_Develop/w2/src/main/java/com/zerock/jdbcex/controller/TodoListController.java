4ac42d489610fcf96b8b4887b2719bc229fe0e73
parkirae <parkirae@kakao.com>
2022-12-13 04:35:19 +0900

    Chore 🧑‍💻: TodoListController servletContext 활용 테스트

--- Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/TodoListController.java
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/controller/TodoListController.java
@@ -23,6 +23,10 @@ public class TodoListController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("todo list...");
 
+        ServletContext servletContext = req.getServletContext();
+
+        log.info("appName: " + servletContext.getAttribute("appName"));
+        
         try {
             List<TodoDTO> dtoList = todoService.listAll();
             req.setAttribute("dtoList", dtoList);
