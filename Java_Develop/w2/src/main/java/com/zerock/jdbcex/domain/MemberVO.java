305cef4b467597bf041b418ffc79a2984cd95eae
parkirae <parkirae@kakao.com>
2022-12-13 02:25:32 +0900

    Feat :art:: MemberVO 추가

---
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/domain/MemberVO.java
@@ -0,0 +1,15 @@
+package com.zerock.jdbcex.domain;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberVO {
+
+    private String mid;
+    private String mpw;
+    private String mname;
+}
