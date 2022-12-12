ccf5465a11ded1e91aac0aa15a15af6d37a8bf33
parkirae <parkirae@kakao.com>
2022-12-13 02:29:34 +0900

    Feat :art:: MemberDTO 추가

---
+++ Java_Develop/w2/src/main/java/com/zerock/jdbcex/dto/MemberDTO.java
@@ -0,0 +1,17 @@
+package com.zerock.jdbcex.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+
+    private String mid;
+    private String mpw;
+    private String mname;
+}
