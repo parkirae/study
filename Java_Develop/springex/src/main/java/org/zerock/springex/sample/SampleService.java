59f304e96d90305f6ddfa4581690bb0667ee101e
parkirae <parkirae@kakao.com>
2022-12-15 17:54:03 +0900

    Feat :art::  SampleService 생성자 주입 방식으로 변경
    Body: 기존 @Autowired에서 변경하였습니다.
    
    @Autowired는 deprecated입니다.

--- Java_Develop/springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,13 +1,14 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @ToString
 @Service
 public class SampleService {
 
-    @Autowired
-    private SampleDAO sampleDAO;
+    private final SampleDAO sampleDAO;
 }
