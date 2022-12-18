5d6268aec54e7a9cab343b0c5aef8a3792fc11ef
parkirae <parkirae@kakao.com>
2022-12-18 15:32:32 +0900

    Refactor ♻️: @RequestMapping을 @GetMapping, @PostMapping으로 분기 처리

--- Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,6 +2,8 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +17,14 @@ public class TodoController {
         log.info("todo list...");
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    //@RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public void register() {
-        log.info("todo register...");
+        log.info("GET todo register...");
+    }
+
+    @PostMapping("/register")
+    public void registerPost() {
+        log.info("POST todo register...");
     }
 }
