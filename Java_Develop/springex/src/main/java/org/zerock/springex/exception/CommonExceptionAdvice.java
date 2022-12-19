ede05716b8b4f714ee301d7b50c72900cf81fc2b
parkirae <parkirae@kakao.com>
2022-12-19 09:41:23 +0900

    Feat :art:: CommonExceptionAdvice 추가
    Body: 해당 클래스는 예외 처리에 사용됩니다.

---
+++ Java_Develop/springex/src/main/java/org/zerock/springex/exception/CommonExceptionAdvice.java
@@ -0,0 +1,11 @@
+package org.zerock.springex.exception;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Log4j2
+@ControllerAdvice
+public class CommonExceptionAdvice {
+
+    
+}
