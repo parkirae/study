cbb4137a696f0e799e7b508054a1e4b69885e392
parkirae <parkirae@kakao.com>
2022-12-19 15:49:46 +0900

    Feat :art::art::art:: TodoServiceImpl 선언

---
+++ Java_Develop/springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
@@ -0,0 +1,30 @@
+package org.zerock.springex.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.TodoDTO;
+import org.zerock.springex.mapper.TodoMapper;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class TodoServiceImpl implements TodoService {
+
+    private final TodoMapper todoMapper;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public void register(TodoDTO todoDTO) {
+
+        log.info(modelMapper);
+
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+
+        log.info(todoVO);
+
+        todoMapper.insert(todoVO);
+    }
+}
