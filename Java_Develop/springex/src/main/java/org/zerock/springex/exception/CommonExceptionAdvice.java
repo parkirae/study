d54647b5142c9c413176d952037543949ca5b057
parkirae <parkirae@kakao.com>
2022-12-19 09:45:23 +0900

    Feat :art:: CommonExceptionAdvice excepNumber() 추가
    Body: excepNumber()는 NumberFormatException을 처리합니다.

--- Java_Develop/springex/src/main/java/org/zerock/springex/exception/CommonExceptionAdvice.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/exception/CommonExceptionAdvice.java
@@ -2,10 +2,19 @@ package org.zerock.springex.exception;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @ControllerAdvice
 public class CommonExceptionAdvice {
 
+    @ResponseBody
+    @ExceptionHandler(NumberFormatException.class)
+    public String excepNumber(NumberFormatException numberFormatException) {
+        log.error("---");
+        log.error(numberFormatException.getMessage());
 
+        return "NUMBER FORMAT EXCEPTION";
+    }
 }
