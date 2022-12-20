44dd38e688b4d7f56b4059a16b1649931ae572f8
parkirae <parkirae@kakao.com>
2022-12-20 09:37:04 +0900

    Test :white_check_mark:: TodoService 테스트
    Body: TodoService register()를 테스트 했습니다.

---
+++ Java_Develop/springex/src/test/java/org/zerock/springex/service/TodoServiceTests.java
@@ -0,0 +1,31 @@
+package org.zerock.springex.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.springex.dto.TodoDTO;
+
+import java.time.LocalDate;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class TodoServiceTests {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister() {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("test")
+                .dueDate(LocalDate.now())
+                .writer("irae")
+                .build();
+
+        todoService.register(todoDTO);
+    }
+}
