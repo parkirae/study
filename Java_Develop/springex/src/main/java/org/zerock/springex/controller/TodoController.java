213b3733f8b9edf42243e164209dc064ca6bfcc6
parkirae <parkirae@kakao.com>
2022-12-20 12:11:13 +0900

    Feat :art::art::art:: TodoController 수정 기능 연결

--- Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -73,4 +73,22 @@ public class TodoController {
 
         return "redirect:/todo/list";
     }
+
+    // 수정
+    @PostMapping("/modify")
+    public String modify(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors...");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("tno", todoDTO.getTno());
+            return "redirect:/todo/modify";
+        }
+
+        log.info(todoDTO);
+
+        todoService.modify(todoDTO);
+
+        return "redirect:/todo/list";
+    }
 }
