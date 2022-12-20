d76a9bc345d5d5362badcbf9cdd0166b32496216
parkirae <parkirae@kakao.com>
2022-12-20 10:00:32 +0900

    Feat :art::art::art:: TodoDTO 검증 처리
    Body: TodoDTO에 @Valid를 적용해 검증을 처리합니다.
    
    검증에 문제가 있다면 다시 입력 화면으로 redirect됩니다.

--- Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -1,18 +1,26 @@
 package org.zerock.springex.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+import org.zerock.springex.service.TodoService;
+
+import javax.validation.Valid;
 
 @Log4j2
 @RequestMapping("/todo")
+@RequiredArgsConstructor
 @Controller
 public class TodoController {
+
+    private final TodoService todoService;
 
     @RequestMapping("/list")
     public void list(Model model) {
@@ -25,8 +33,16 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes) {
+    public String registerPost(@Valid TodoDTO todoDTO, RedirectAttributes redirectAttributes, BindingResult bindingResult) {
         log.info("POST todo register...");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors...");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/todo/register";
+        }
+
         log.info(todoDTO);
         return "redirect:/todo/list";
     }
