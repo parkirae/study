ccebac9d0edbae454bca17916ceeca10a5bf242c
parkirae <parkirae@kakao.com>
2022-12-20 09:45:08 +0900

    Feat :art::art::art:: TodoController /register Post 분기 처리
    Body: registerPost()는 /register POST 요청 시 TodoDTO에 담아 데이터를 저장하고 /todo/list로 redirect합니다.

--- Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -4,7 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.springex.dto.TodoDTO;
 
 @Log4j2
 @RequestMapping("/todo")
@@ -19,5 +22,12 @@ public class TodoController {
     @GetMapping("/register")
     public void registerGET() {
         log.info("GET todo register...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes) {
+        log.info("POST todo register...");
+        log.info(todoDTO);
+        return "redirect:/todo/list";
     }
 }
