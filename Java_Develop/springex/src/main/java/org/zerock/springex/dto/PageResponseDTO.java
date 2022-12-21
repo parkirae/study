1cb56b9d52f2726bd7f1048901cb8c129e89b3bf
parkirae <parkirae@kakao.com>
2022-12-21 09:30:38 +0900

    Feat :art:: PageRequestDTO 마지막, 시작, 이전, 다음 페이지 계산 추가

--- Java_Develop/springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
+++ Java_Develop/springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
@@ -35,5 +35,20 @@ public class PageResponseDTO<E> {
 
         this.total = total;
         this.dtoList = dtoList;
+
+        // 마지막 페이지
+        this.end = (int)(Math.ceil(this.page / 10.0)) * 10;
+
+        // 시작 페이지
+        this.start = this.end - 9;
+
+        // 마지막 페이지 개수
+        int last = (int)(Math.ceil((total / (double)size)));
+
+        // 이전 페이지
+        this.prev = this.start > 1;
+
+        // 다음 페이지
+        this.next = total > this.end * this.size;
     }
 }
