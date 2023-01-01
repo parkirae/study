a0cf413032a867fff89e00719681c687f1d1a44b
parkirae <parkirae@kakao.com>
2023-01-01 17:00:18 +0900

    Feat :art:: 엔티티 클래스(Board)에 BaseEntity 상속 외 어노테이션 추가

--- Java_Develop/b01/src/main/java/org/zerock/b01/domain/Board.java
+++ Java_Develop/b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -1,12 +1,19 @@
 package org.zerock.b01.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
-public class Board {
+public class Board extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
